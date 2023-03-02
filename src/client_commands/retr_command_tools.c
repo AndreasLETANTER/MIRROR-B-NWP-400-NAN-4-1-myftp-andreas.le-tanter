0ba5483112279f439738137b485d587326c8494f
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-02 16:24:34 +0100

    [FIX]: norm for cleaner code

--- src/client_commands/retr_command_tools.c
+++ src/client_commands/retr_command_tools.c
@@ -73,11 +73,13 @@ void retr_engine(int sd, socket_info_s *_socket_info, char *arg)
 
     if (check_error(sd, _socket_info, arg) == -1)
         return;
-    
-    path = malloc(sizeof(char) * strlen(_socket_info->current_directory) + strlen(arg) + 1);
+
+    path = malloc(sizeof(char) * strlen(_socket_info->current_directory)
+        + strlen(arg) + 1);
     strcat(path, _socket_info->current_directory);
     strcat(path, arg);
     filecontent = open_file(arg);
+    free(path);
     if (filecontent == NULL) {
         custom_write(sd, "550 Requested action not taken. \
 File unavailable\n");
