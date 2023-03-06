97e6632b9775b0217e6641ffea9b4733dc7c3de7
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-06 13:36:20 +0100

    [ADD]: handling of -help for displaying info on the program

--- src/main.c
+++ src/main.c
@@ -10,6 +10,12 @@
 
 int main (int argc, char **argv)
 {
+    if (argc == 2 && strcmp(argv[1], "-help") == 0) {
+        printf("USAGE: ./myftp port path\n");
+        printf("\tport is the port number on which the server socket listens\n");
+        printf("\tpath is the path to the home directory for the Anonymous user\n");
+        return (0);
+    }
     if (argc == 3) {
         server_engine(argv[1], argv[2]);
         return (0);
