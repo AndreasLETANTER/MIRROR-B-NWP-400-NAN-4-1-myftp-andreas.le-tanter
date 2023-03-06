91b2f1f281ed276a8c11da0113b140efa298b097
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-06 20:02:25 +0100

    [ADD]: reply code for user and pass command

--- src/client_commands/user_login_commands.c
+++ src/client_commands/user_login_commands.c
@@ -29,9 +29,6 @@ void check_login(int sd_idx, socket_info_s *_socket_info)
 {
     if (_socket_info->client_socket[sd_idx]->is_logged == true) {
         return;
-    } else if (_socket_info->client_socket[sd_idx]->current_user == NULL) {
-        custom_write(_socket_info->client_socket[sd_idx]->socket_fd, "530 Not logged in.\n");
-        return;
     }
     if (strcmp(_socket_info->client_socket[sd_idx]->current_user, "Anonymous") == 0 && _socket_info->client_socket[sd_idx]->current_pswd == NULL) {
         _socket_info->client_socket[sd_idx]->is_logged = true;
@@ -46,12 +43,17 @@ void handle_pass_command(int sd_idx, socket_info_s *_socket_info, char *arg)
     int sd = _socket_info->client_socket[sd_idx]->socket_fd;
     char *nextparam = strtok(NULL, " ");
 
+    if (_socket_info->client_socket[sd_idx]->current_pswd != NULL) {
+        free(_socket_info->client_socket[sd_idx]->current_pswd);
+    } else if (_socket_info->client_socket[sd_idx]->current_user == NULL) {
+        custom_write(sd, "332 Need account for login.\n");
+        return;
+    }
     if (arg == NULL) {
         custom_write(sd, "331 Password okay.\n");
         check_login(sd_idx, _socket_info);
         return;
     }
-
     _socket_info->client_socket[sd_idx]->current_pswd = strdup(arg);
     custom_write(sd, "331 Password okay.\n");
     check_login(sd_idx, _socket_info);
