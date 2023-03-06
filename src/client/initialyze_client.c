631e89a4680819b694c756949d584a4f597901ce
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-06 20:03:29 +0100

    [ADD]: reset of the logged boulean checking for user logged or not

--- src/client/initialyze_client.c
+++ src/client/initialyze_client.c
@@ -12,6 +12,7 @@ void reset_client(socket_info_s *_socket_info, int sd_idx, int new_socket)
     _socket_info->client_socket[sd_idx]->socket_fd = new_socket;
     _socket_info->client_socket[sd_idx]->data_client = -1;
     _socket_info->client_socket[sd_idx]->data_socket = -1;
+    _socket_info->client_socket[sd_idx]->is_logged = false;
     _socket_info->client_socket[sd_idx]->socket_type = CLIENTSOCKET;
 }
 
